88590c86760f34c097db5b5822187ff634bdc8a1
vivostanoev <vivostanoev@gmail.com>
2020-11-10 17:09:14 +0200

    add files to gitgub

---
+++ src/test/java/Base.java
@@ -0,0 +1,21 @@
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Allure;
+import org.junit.After;
+import org.junit.Before;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Base {
+
+    @Before
+    public void beforeTest() {
+        Allure.step("Open a browser dialog");
+        open("https://www.demoblaze.com/");
+    }
+
+    @After
+    public void afterTest()
+    {
+        WebDriverRunner.closeWindow();
+    }
+}
---
+++ src/test/java/OpenBrowserTests.java
@@ -0,0 +1,28 @@
+import io.qameta.allure.Allure;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class OpenBrowserTests extends Base{
+
+    @Test
+    @DisplayName("Test Opening browser dialog")
+    public void openBrowser() throws Exception
+    {
+        Allure.step("Click menu item 'Laptops'");
+        $(withText("Laptops")).click();
+        $(withText("MacBook air")).click();
+    }
+
+
+    @Test
+    @DisplayName("Test expected failure")
+    public void openBrowserAndFailTest()
+    {
+        Allure.step("I want to fail the test");
+        Assert.assertTrue(false);
+    }
+}
